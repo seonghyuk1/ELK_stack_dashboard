c04705ad177b231785c069de0dfced5c107273e6
Uddiyam <97297404+Uddiyam@users.noreply.github.com>
2023-01-25 20:33:46 +0900

    react 대시보드

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <>
     <App />
-  </BrowserRouter>
+  </>
 );
